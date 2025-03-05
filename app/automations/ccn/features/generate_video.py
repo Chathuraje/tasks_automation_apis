619465c59b58ada5f205ad28f25c3bc719c6f337
Chathura J Ekanayake <chathuraje01@gmail.com>
2025-03-06 03:56:39 +0530

    Remove unused code and update video generation

--- app/automations/ccn/features/generate_video.py
+++ app/automations/ccn/features/generate_video.py
@@ -1,13 +1,11 @@
 import json
 import os
-from app.automations.ccn.models.news_model import VideoResponse
+import random
 from app.features.gdown import download_file_from_drive
 from fastapi import HTTPException
-from moviepy import *
 from app.features.ffmpeg.video import merge_audio_video
-from app.features.ffmpeg.tools import get_audio_duration
-from app.features.ffmpeg.subtitle import add_permanent_subtitles, convert_srt_to_ass
-import random
+from app.features.ffmpeg.subtitle import add_permanent_subtitles
+
 from app.features.google_drive.drive_upload import upload_to_drive
 from app.features.google_drive.tools import file_exists
 
