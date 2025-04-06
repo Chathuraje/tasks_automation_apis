0929b07d24bac90807373e1293d989bada37b95a
Chathura J Ekanayake <chathuraje01@gmail.com>
2025-04-07 00:39:02 +0530

    adding config.py to route template

---
+++ templates/route_template/config.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+
+# Configuration settings for the {Name}
+class CONFIG:
+    # {Short_name}_PROXY_USERNAME = os.getenv("{Short_name}_PROXY_USERNAME")
+    # {Short_name}_PROXY_PASSWORD = os.getenv("{Short_name}_PROXY_PASSWORD")
+    pass
+
+
+config = CONFIG()
